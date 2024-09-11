d7b94d78783b8aa5d69a3e1a6251f307d76e5b8c
didge <mattg4@gmail.com>
2024-09-11 19:09:29 +0100

    flex added

---
+++ flex_cron.py
@@ -0,0 +1,47 @@
+
+
+from selenium import webdriver #default requirement
+from selenium.webdriver.common.by import By # this is used to find elements on the page to interact with
+from selenium.webdriver.chrome.options import Options #for adding options such as headless
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+import time, sys, re, os
+import psutil
+import asyncio
+from telegram import Bot
+from data import process_data
+from scape import get_url, kill_chrome_processes, powercity
+
+
+
+# Function to send message to Telegram
+async def send_message_to_telegram(message):
+    # Replace 'YOUR_BOT_TOKEN' with your actual bot token
+    bot_token = '6911991203:AAGuVx7-e57Aniie7YvaOswF2yR7knPd9hM'
+    # Replace 'YOUR_CHAT_ID' with the chat ID you want to send messages to
+    chat_id = '-4045954869'
+    bot = Bot(token=bot_token)
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
+
+def main(url):
+    
+    kill_chrome_processes()
+    driver, wait = get_url(url)
+    powercity(driver,wait)
+            
+    message = 'Hex Flex Protocol\n'
+    message += '--------------------\n'
+    message += process_data('0x22Cf...7fcd','rolling_private')
+    message += process_data('0x6AfB...e57E', 'liquid_orange')
+    
+    #send data to telegram
+    asyncio.run(send_message_to_telegram(message))
+    
+if __name__ == "__main__":
+    main('https://www.flex.powercity.io/#/')
+    
